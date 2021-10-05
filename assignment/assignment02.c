efb9c6bb87ca59ccc18d3aa9fbaf78a614674433
toysin <toysin1203@hotmail.com>
2021-10-05 18:33:03 +0900

    assignment02 done

--- assignment/assignment02.c
+++ assignment/assignment02.c
@@ -13,10 +13,29 @@ typedef struct Heap
 	Node	*root;
 }			Heap;
 
+typedef struct QNode
+{
+	Node			*node;
+	struct QNode	*link;
+}					QNode;
+
+typedef struct Queue
+{
+	QNode	*front;
+	QNode	*rare;
+}			Queue;
+
 Node	*getNode();
-Node	*rBuildHeap(int n, int cur);
+`
+Node	*rBuildHeap(int n, int cur, Queue *q);
+Node	*BuildHeap(int n);
 void	downHeap(Node *node);
-void	printHeap(Heap *heap);
+void	printHeap(Node *node);
+
+QNode	*getQNode();
+void	enQueue(Queue *q, Node *node);
+Node	*deQueue(Queue *q);
+int		isEmptyQueue(Queue *q);
 
 void	swapKey(int *a, int *b);
 
@@ -31,8 +50,8 @@ int	main(void)
 	if (!(heap = (Heap *)malloc(sizeof(Heap))))
 		return 0;
 	scanf("%d", &n);
-	rBuildHeap(n, 1);
-	printHeap(heap);
+	heap->root = BuildHeap(n);
+	printHeap(heap->root);
 	freeHeap(heap);
 }
 
@@ -46,26 +65,48 @@ Node	*getNode()
 	return node;
 }
 
-Node	*rBuildHeap(int n, int cur)
+Node	*rBuildHeap(int n, int cur, Queue *q)
 {
 	Node	*parent, *left, *right;
-	
+	int		temp = 1;
+
 	left = NULL;
 	right = NULL;
-
-	parent = getNode();
-	scanf("%d", &parent->key);
-	if (n == cur)
-		return parent;
 	
-	if (n >= cur * 2)
-		left = rBuildHeap(n, cur * 2);
-	if (n >= cur * 2 + 1)
-		right = rBuildHeap(n, cur * 2 + 1);
-	parent->left = left;
-	parent->right = right;
+	if (cur == 1)
+	{
+		parent = getNode();
+		scanf("%d", &(parent->key));
+		enQueue(q, parent);
+	}
+	parent = deQueue(q);
+	if (cur * 2 <= n)
+	{
+		left = getNode();
+		scanf("%d", &(left->key));
+		parent->left = left;
+		enQueue(q, left);
+	}
+	if (cur * 2 + 1 <= n)
+	{
+		right = getNode();
+		scanf("%d", &(right->key));
+		parent->right = right;
+		enQueue(q, right);
+	}
+	if (n >= cur + 1)
+		rBuildHeap(n, cur + 1, q);
 	downHeap(parent);
 	return parent;
+}
+
+Node	*BuildHeap(int n)
+{
+	Queue	q;
+	q.front = NULL;
+	q.rare = NULL;
+	
+	return rBuildHeap(n, 1, &q);
 }
 
 void	downHeap(Node *node)
@@ -101,10 +142,70 @@ void	downHeap(Node *node)
 	}
 }
 
-void	printHeap(Heap *heap)
+void	printHeap(Node *node)
 {
-	//code
+	Queue	*q = (Queue *)malloc(sizeof(Queue));
+	Node	*temp;
+	
+	if (q == NULL)
+		return;
+	q->front = NULL;
+	q->rare = NULL;
+	enQueue(q, node);
+	while (!isEmptyQueue(q))
+	{
+		temp = deQueue(q);
+		printf(" %d", temp->key);
+		if (temp->left)
+			enQueue(q, temp->left);
+		if (temp->right)
+			enQueue(q, temp->right);
+	}
+	free(q);
 }
+
+
+QNode	*getQNode()
+{
+	QNode	*node = (QNode *)malloc(sizeof(QNode));
+	if (node == NULL)
+		return 0;
+	node->link = NULL;
+	return node;
+}
+
+void	enQueue(Queue *q, Node *node)
+{
+	QNode	*qn = getQNode();
+	qn->node = node;
+	if (isEmptyQueue(q))
+	{
+		q->front = qn;
+		q->rare = qn;
+	}
+	else
+	{
+		q->rare->link = qn;
+		q->rare= qn;
+	}
+}
+
+Node	*deQueue(Queue *q)
+{
+	Node *temp = q->front->node;
+	QNode *tmp = q->front;
+	q->front = q->front->link;
+	if (q->front == NULL)
+		q->rare = NULL;
+	free(tmp);
+	return temp;
+}
+
+int	isEmptyQueue(Queue *q)
+{
+	return !(q->front);
+}
+
 
 void	swapKey(int *a, int *b)
 {
