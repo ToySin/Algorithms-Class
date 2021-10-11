8f939d2e214200eafa650ef43d2bb53804876fa6
toysin <toysin1203@hotmail.com>
2021-10-11 11:59:32 +0900

    Create avltree.c

---
+++ practice/week07/avltree.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node
+{
+	int	key;
+	int height;
+	struct Node	*parent;
+	struct Node	*left;
+	struct Node	*right;
+}Node;
+
+typedef struct Tree
+{
+	Node	*root;
+}Tree;
