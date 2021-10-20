1ab2f1c78f2622a77648252d1a28acdfcfd103f0
toysin <toysin1203@hotmail.com>
2021-10-20 22:46:09 +0900

    Update dict1.c

--- practice/week06/dict1.c
+++ practice/week06/dict1.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void	createDict(int **pdict, int n);
+void	insertKey(int *dict, int n);
 int		searchKey(int *dict, int key, int start, int end);
 
 int	main(void)
@@ -9,12 +11,37 @@ int	main(void)
 	int	n, k;
 
 	scanf("%d %d", &n, &k);
-	if (!(dict = (int *)malloc(sizeof(int) * n)))
-		return (0);
+	dict = (int *)malloc(sizeof(int) * n);
 	for (int i = 0; i < n; i++)
-		scanf("%d", &dict[i]);
+		insertKey(dict, i);
 	printf("%d", searchKey(dict, k, 0, n - 1));
 	free(dict);
+}
+
+void	createDict(int **pdict, int n)
+{
+	if (!(*pdict = (int *)malloc(sizeof(int) * n)))
+		return;
+}
+
+void	insertKey(int *dict, int n)
+{
+	int key;
+
+	scanf("%d", &key);
+	
+	for (int i = n; i >= 0; i--)
+	{
+		if (!i)
+			dict[0] = key;
+		else if (dict[i - 1] <= key)
+		{
+			dict[i] = key;
+			break;
+		}
+		else
+			dict[i] = dict[i - 1];
+	}
 }
 
 int	searchKey(int *dict, int key, int start, int end)
@@ -22,7 +49,7 @@ int	searchKey(int *dict, int key, int start, int end)
 	int mid = (end + start) / 2;
 
 	if (start > end)
-		return (end < 0 ? -1 : end);
+		return (end <= 0 ? -1 : end);
 	if (dict[mid] == key)
 		return (mid);
 	else if (dict[mid] > key)
