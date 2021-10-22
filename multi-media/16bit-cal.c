0be83704da2103d78982eb88d12ed93a091ef108
toysin <toysin1203@hotmail.com>
2021-10-22 22:16:27 +0900

    Create 16bit-cal.c

---
+++ multi-media/16bit-cal.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void	binary(int n)
+{
+	if (n > 1)
+		binary(n / 2);
+	printf("%d", n % 2);
+}
+
+int main()
+{
+	int n;
+
+	scanf("%d", &n);
+
+	binary(n);
+	printf("\n");
+
+	return 0;
+}
