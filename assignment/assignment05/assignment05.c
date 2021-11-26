a10426c112583041e659e93bb69f5cc1437ce284
toysin <toysin1203@hotmail.com>
2021-11-26 15:46:33 +0900

    Update assignment05.c

--- assignment/assignment05/assignment05.c
+++ assignment/assignment05/assignment05.c
@@ -3,18 +3,27 @@
 
 typedef struct Vertex
 {
+	// 정점의 이름
 	int				v_name;
+	// 인접리스트 형태로 구현
 	struct Vertex	*inc;
 }					Vertex;
 
+// 그래프의 전체적인 구성은 다음과 같습니다.
+// 각 정점에 대한 접근은 오로지 인덱스로만 접근합니다.(정점 자체를 저장하지 않음)
+// Vertex *vertices[]는 인덱스로 접근한 각 정점의 인접 정점을 저장합니다.
+// 헤더는 없으며 연결된 정점이 없으면 NULL입니다.(초기화)
 typedef struct Graph
 {
 	int		v_size;
+	// 정점의 방문정보를 저장하는 배열
 	int		visited[100];
 	Vertex	*vertices[100];
+	// 정점에 속한 간선의 수를 저장하는 배열
 	int		inc_count[100];
 }			Graph;
 
+// 인접 정점을 연결하기 위해 할당
 Vertex	*get_vertex()
 {
 	Vertex	*node = (Vertex *)malloc(sizeof(Vertex));
@@ -32,6 +41,7 @@ void	init_graph(Graph *graph)
 	}
 }
 
+// 각 정점에 대한 접근은 인덱스로만 접근합니다.
 void	insert_vertex(Graph *graph)
 {
 	graph->v_size++;
@@ -41,15 +51,18 @@ void	insert_edge(Graph *graph, int u, int v)
 {
 	Vertex	*temp;
 
+	// 둘다 있는 정점인 경우
 	if (graph->v_size < u || graph->v_size < v)
 		return ;
 
+	// u인덱스 위치에 v정점을 연결하는 작업
 	temp = get_vertex();
 	temp->v_name = v;
 	temp->inc = graph->vertices[u - 1];
 	graph->vertices[u - 1] = temp;
 	graph->inc_count[u - 1]++;
 
+	// v인덱스 위치에 u정점을 연결하는 작업
 	temp = get_vertex();
 	temp->v_name = u;
 	temp->inc = graph->vertices[v - 1];
@@ -69,11 +82,8 @@ int	rDFS(Graph *graph, int v)
 	cycle_flag = 1;
 	while (temp)
 	{
-		// 방문하기전에 이미 방문했는지를 본다.
-		// 만약 방문할 곳이 없는데, inc_count가 2 이상이면
-		// 사이클이다.
-		// flag 하나 세워서 중간에 한번이라도 재귀 안돌면
-		// 마지막 가서 검사?
+		// 만약 방문할 곳이 없는데, inc_count가 2 이상이면 사이클이다.
+		// inc_count가 1인 경우는 leaf인 경우이다.
 		if (!graph->visited[temp->v_name - 1])
 		{
 			if (!rDFS(graph, temp->v_name))
@@ -89,11 +99,16 @@ int	rDFS(Graph *graph, int v)
 
 int	DFS(Graph *graph)
 {
+	// 실행전에 방문정보를 초기화한다.
 	for (int i = 0; i < graph->v_size; i++)
 		graph->visited[i] = 0;
 	return (rDFS(graph, 1));
 }
 
+// DFS로 방문정보만 업데이트 해주면 된다.
+// 사이클을 탐색해야되는 위의 함수와는 차이가 있다.
+// 위의 함수는 방문정보를 확인 후 재귀를 실행했지만,
+// 이 함수는 방문 후 방문정보를 확인하고 리턴한다.
 void	rcount_island(Graph *graph, int v)
 {
 	int		v_idx;
@@ -111,6 +126,10 @@ void	rcount_island(Graph *graph, int v)
 	}
 }
 
+// 기본적으로 DFS형태로 동작하는 함수이다.
+// 다만 한번의 DFS가 끝난 이후, 전체 정점에서 방문하지 않은 정점이 있을 경우
+// 그 정점에서 다시 DFS를 시작한다.
+// 방문정보는 기존의 정보를 유지하면서 실행한다.
 int	count_island(Graph *graph)
 {
 	int	island;
@@ -129,6 +148,7 @@ int	count_island(Graph *graph)
 	return (island);
 }
 
+// inc_count 배열에서 최대값을 반환하기 위한 함수이다.
 int	get_max(int *arr, int n)
 {
 	int	max_idx;
@@ -140,6 +160,8 @@ int	get_max(int *arr, int n)
 	return (arr[max_idx]);
 }
 
+// 중심을 출력하고 이심률을 구해서 리턴하는 함수이다.
+// 정점이 한개인 경우엔 이심률이 0이라고 가정하였다.
 int	get_eccentricity(Graph *graph)
 {
 	Vertex	*temp;
@@ -147,6 +169,17 @@ int	get_eccentricity(Graph *graph)
 	int		leaf_count;
 	int		eccentricity;
 
+	// 마지막에는 정점이 하나 남거나, 두개가 남는다.
+	// 인접 정점의 수가 1이 최대가 될때가 끝이 난 상황이다.
+	// 매번 시행마다 잎의 개수가 다를 수 있으므로 따로 leaf_count를 세어준다.
+	// 잎 위치에 있는 정점들을 제거하는 가정을 하면서
+
+	// 1. inc_count가 1인 잎들만 조사한다.
+	// 3. 그 잎들의 부모 위치의 정점을 leaf_parent_index_list에
+	//		인덱스 값으로 저장한다.
+	// 4. 제거 된 잎들은 inc_count가 0으로 고정된다.
+	//
+	// inc_count의 개수를 하나씩 줄여나간다.
 	eccentricity = 0;
 	while (get_max(graph->inc_count, graph->v_size) > 1)
 	{
@@ -156,8 +189,13 @@ int	get_eccentricity(Graph *graph)
 			if (graph->inc_count[i] == 1)
 			{
 				temp = graph->vertices[i];
+
 				while (temp)
 				{
+					// 잎을 실제로 제거한 것이 아니기 때문에
+					// 1-2-3-... 으로 연결 된 그래프가 있다면
+					// 1을 먼저 제거하면 (1)-2-3-... 에서 2가 잎이 되는데
+					// (1)은 이미 제거 되었으므로 조사 대상에서 제외합니다.
 					if (graph->inc_count[temp->v_name - 1])
 					{
 						leaf_parent_index_list[leaf_count] = temp->v_name - 1;
@@ -168,6 +206,10 @@ int	get_eccentricity(Graph *graph)
 				graph->inc_count[i] = 0;
 			}
 		}
+		// 이번 조사에서 얻은 정보로 inc_count를 갱신합니다.
+		// 다만 값을 줄이는 도중 해당 정점의 inc_count가 1이 되면
+		// 더이상 줄이지 않습니다. 1-2-1 같은 경우에는 위 조건이 없을 시
+		// 0-0-0이 됩니다. 0-1-0의 형태로 만들어 주기 위함입니다. (중심이 1개)
 		for (int j = 0; j < leaf_count; j++)
 			if (graph->inc_count[leaf_parent_index_list[j]] > 1)
 				graph->inc_count[leaf_parent_index_list[j]]--;
@@ -182,9 +224,10 @@ int	get_eccentricity(Graph *graph)
 			printf("%d ", i + 1);
 		}
 	}
+	// 중심이 두개라면 이심률을 1 더해줍니다.
+	// 중심 서로를 거쳐가는 경로 +1
 	if (leaf_count)
 		eccentricity++;
-	printf("\n%d", eccentricity);
 	return (eccentricity);
 }
 
@@ -203,19 +246,25 @@ int	main()
 		insert_edge(&graph, u, v);
 	}
 
+	// 사이클 여부를 먼저 검사합니다.
+	// 사이클이 없으면 1이 리턴됩니다.
 	if (DFS(&graph))
 	{
+		// 방문하지 않은 정점이 있는지 확인합니다.
 		for (int i = 0; i < graph.v_size; i++)
 			if (!graph.visited[i])
 			{
+				// 사이클은 없지만, 인접요소가 2개 이상인 경우 동작합니다.
 				printf("%d", count_island(&graph));
 				return (0);
 			}
-		get_eccentricity(&graph);
+		// 트리 구조인 경우 동작합니다.
+		printf("%d", get_eccentricity(&graph));
 		return (0);
 	}
 	else
 	{
+		// 사이클이 있는 경우 동작합니다.
 		printf("%d", count_island(&graph));
 		return (0);
 	}
